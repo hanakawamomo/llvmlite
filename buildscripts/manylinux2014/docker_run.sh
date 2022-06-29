48b62b045e24f9f1910694ee5156d3b5475c5198
Yuyan Jiang <yuyanj@dropbox.com>
2022-06-29 09:20:09 +0000

    pin auditwheel 4

--- buildscripts/manylinux2014/docker_run.sh
+++ buildscripts/manylinux2014/docker_run.sh
@@ -4,4 +4,4 @@ srcdir=$( cd "$(dirname $0)/../.."  && pwd )
 echo "srcdir=$srcdir"
 
 echo "MINICONDA_FILE=$MINICONDA_FILE"
-docker run -it -e "ARCH=$ARCH" -v $srcdir:/root/llvmlite quay.io/pypa/manylinux2014_${ARCH} ${PRECMD} /root/llvmlite/buildscripts/manylinux2014/$1 ${MINICONDA_FILE} $2
+docker run -it -e "ARCH=$ARCH" -v $srcdir:/root/llvmlite quay.io/pypa/manylinux2014_${ARCH}@sha256:75ba3376a63a4d90e8fce2425d2cf8a2edee6e2715d306993780cbade3b73e44 ${PRECMD} /root/llvmlite/buildscripts/manylinux2014/$1 ${MINICONDA_FILE} $2
